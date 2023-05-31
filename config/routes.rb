fa189309550a495c0055b8a850b0a5cfde54fc30
massa1111 <t09071460846@gmail.com>
2023-05-31 15:43:03 +0900

    コメント表示完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   
   root to: "items#index"
   resources :items do
+    resources :comments, only: :create
     resources :orders, only: [:create, :index]
   end
 end
