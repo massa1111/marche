e5fe5f4fc83e5bd831b706f3656170eee0b22301
massa1111 <t09071460846@gmail.com>
2023-05-31 21:28:40 +0900

    ユーザー詳細ページビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   get 'items/index'
   get 'items/item_list', to: 'items#item_list', as: 'items_item_list'
   get 'users/:id', to: 'users#show', as: 'user'
+  
   
   root to: "items#index"
   resources :items do
