799c7a66eb514eae800198a79ac7660bbe0797d0
massa1111 <t09071460846@gmail.com>
2023-06-01 17:22:15 +0900

    ユーザー詳細ページ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   get 'items/index'
   get 'items/item_list', to: 'items#item_list', as: 'items_item_list'
   get 'users/:id', to: 'users#show', as: 'user'
